930251bd78ae7f1edcc72e15a980750a70d63131
Florian <florian.souzeau@gmail.com>
2026-02-13 18:51:19 +0100

    Initial Commit

---
+++ main.py
@@ -0,0 +1,17 @@
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+
+
+
+app.run(host='192.168.3.16', port=81)
