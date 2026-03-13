238f30886cbb87bc0f3e0f282eb496a15067d57a
Florian <florian.souzeau@gmail.com>
2026-03-13 19:17:05 +0100

    Added questions

--- main.py
+++ main.py
@@ -1,17 +1,46 @@
 
-from flask import Flask, render_template
+from flask import Flask, render_template, session
+from questions import questions
+import os
 
 app = Flask(__name__)
+
+app.secret_key = os.urandom(32)
 
 
 
 
 @app.route('/')
 def index():
+
+    session["numero_question"] = 0
+
+    session["score"] = {"Sw": 0, "Ro": 0, "Ka": 0, "Me": 0}
+
+
     return render_template('index.html')
 
+@app.route('/question')
+def question():
+    global questions
 
+    numero = session["numero_question"]
 
+    if numero < len(questions):
+        enonce_question = questions[numero]["enonce"]
+
+        symboles_et_reponse = questions[numero].copy()
+
+        symboles_et_reponse.pop("enonce")
+
+        reponses = list(symboles_et_reponse.values())
+        symboles = list(symboles_et_reponse.keys())
+
+        session["symboles"] = symboles
+
+        return render_template('question.html', enonce = enonce_question, reponses = reponses, symboles = symboles)
+    else:
+        return render_template('resultat.html')
 
 
 app.run(host='192.168.3.16', port=81)
---
+++ questions.py
@@ -0,0 +1,45 @@
+
+questions = [
+    {
+        "enonce": "Avec des amis, tu préfères :",
+        "Sw": "Profiter du moment présent et apprécier la compagnie de tes amis, même si vous ne faites rien de spécial.", 
+        "Ro": "Faire quelque chose de stimulant et amusant, comme jouer à des jeux vidéo ou faire du sport.", 
+        "Ka": "Faire quelque chose de calme et relaxant, comme lire un livre ou écouter de la musique.", 
+        "Me": "Passer du temps en solo, en faisant ce que tu aimes."
+    },
+    {
+        "enonce": "Dans les jeux, tu prefères :",
+        "Sw": "Aller en mélée et découper tes ennemis tout en étant équilibré.", 
+        "Ro": "Etre de loin et tout faire exploser (même toi).", 
+        "Ka": "Jouer défensif, être strategique et faire de gros dégats d'un coup.", 
+        "Me": "Faire attention à tes alliés, être celui qui garde tes equipiers en vie."
+    },
+    {
+        "enonce": "Si tu dois faire quelque chose pour quelqu'un :",
+        "Sw": "Tant que c'est ton ami, tu l'aideras toujours.", 
+        "Ro": "Si c'est pas fun, c'est pas pour toi.", 
+        "Ka": "Tant que tu peux être tranquille en la faisant, tu acceptes toujours la tache.", 
+        "Me": "Tu n'aides que si tu en gagnes quelque chose."
+    },
+    {
+        "enonce": "Lorque tu regardes un paysage :",
+        "Sw": "Tu sais apprécier le moment, surtout si c'est avec des gens que tu aimes.", 
+        "Ro": "Après 2 secondes, tu penses déjà à autre chose.", 
+        "Ka": "Tu pourrais dormir là-bas si la vue est belle.", 
+        "Me": "Tu apprécierais sans forcement faire attention, en pensant à autre chose."
+    },
+    {
+        "enonce": "Quand tu est tout seul, tu préfères :",
+        "Sw": "Dormir un peu ou te reposer pour la prochaine fois que tu devras faire quelque chose.", 
+        "Ro": "Faire quelque chose de stimulant, comme du sport ou écouter de la musique, où tu vas checrher quelqu'un avec qui passer le moment.", 
+        "Ka": "Te relaxer devant une série ou t'assoir quelque part à rêvasser.", 
+        "Me": "Faire quelque chose de productif, comme travailler ou penser à ce que tu feras plus tard."
+    },
+    {
+        "enonce": "Tu dirais que tu es plus :",
+        "Sw": "Amical et Joyeux.", 
+        "Ro": "Superactif et gentil.", 
+        "Ka": "Calme et bienveillant.", 
+        "Me": "Radin et intelligent."
+    },
+]
